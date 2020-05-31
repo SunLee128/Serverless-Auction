5253ed36074cdcbe32e3aa863a52fbcb6d1a0045
sunlee <sunlee128@gmail.com>
2020-05-31 16:52:09 +1000

    add placeBid

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -13,10 +10,13 @@ async function createAuction (event, context) {
   const now = new Date();
 
   const auction = {
-    id        : uuid(),
+    id         : uuid(),
     title,
-    status    : 'OPEN',
-    createdAt : now.toISOString()
+    status     : 'OPEN',
+    createdAt  : now.toISOString(),
+    highestBid : {
+      amount : 0
+    }
   };
 
   try {
@@ -36,7 +36,4 @@ async function createAuction (event, context) {
   };
 }
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(createAuction);
